6c87887ed95a8030e8f0179a1d5e6a0960a7528f
Zone1411-HK <kevinke2005@gmail.com>
2026-04-15 10:28:27 +0200

    kész

--- backend/api/api.js
+++ backend/api/api.js
@@ -41,4 +41,52 @@ router.get('/testsql', async (request, response) => {
     }
 });
 
+router.get('/brands', async (req, res) => {
+    let allCars = await database.allCars();
+    let result = {};
+
+    for (let car of allCars) {
+        if (!result[car.brand]) {
+            result[car.brand] = 1;
+        } else {
+            result[car.brand] += 1;
+        }
+    }
+
+    console.log(result);
+    res.status(200).json(result);
+});
+
+router.get('/salaries', async (req, res) => {
+    let allEmployees = await database.allEmployees();
+    let result = {};
+
+    for (let employee of allEmployees) {
+        if (!result[employee.department]) {
+            result[employee.department] = employee.salary;
+        } else {
+            result[employee.department] += employee.salary;
+        }
+    }
+
+    console.log(result);
+    res.status(200).json(result);
+});
+
+router.get('/revenue', async (req, res) => {
+    let allPurchases = await database.allPurchases();
+    let result = {};
+
+    for (let purchase of allPurchases) {
+        let revenue = purchase.price * purchase.quantity;
+        if (!result[purchase.category]) {
+            result[purchase.category] = revenue;
+        } else {
+            result[purchase.category] += revenue;
+        }
+    }
+
+    res.status(200).json(result);
+});
+
 module.exports = router;
--- backend/sql/database.js
+++ backend/sql/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
     password: '',
-    database: 'exampledb',
+    database: 'statisztika',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
@@ -16,7 +16,26 @@ async function selectall() {
     const [rows] = await pool.execute(query);
     return rows;
 }
+
+async function allCars() {
+    const [rows] = await pool.execute(`SELECT * FROM vehicles`);
+    return rows;
+}
+
+async function allEmployees() {
+    const [rows] = await pool.execute(`SELECT * FROM employees WHERE is_active = 1 `);
+    return rows;
+}
+
+async function allPurchases() {
+    const [rows] = await pool.execute(`SELECT * FROM purchases `);
+    return rows;
+}
+
 //!Export
 module.exports = {
-    selectall
+    selectall,
+    allCars,
+    allEmployees,
+    allPurchases
 };
